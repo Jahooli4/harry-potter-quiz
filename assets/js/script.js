81e4efeea3746d95cc3537224f5744c8f815abd5
Michelle Jones <michelle.jones.2161@hotmail.co.uk>
2024-08-27 12:29:41 +0000

    define js variables and write out questions and answer options

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,93 @@
 const home = document.getElementById("home-page");
 
+const question = document.getElementById("question-text");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+
+let currentQuestion = {};
+let acceptingAnsers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestioms = [];
+
+let questions = [
+    {
+        question: "In which Hogwarts house is Harry Potter?",
+        choice1: "Slytherin",
+        choice2: "Gryffindor",
+        choice3: "Hufflepuff",
+        choice4: "Ravenclaw",
+        answer: 2
+    },
+    {
+        question: "The juice of which fruit or vegetable is commonly served at Hogwarts?",
+        choice1: "Apple",
+        choice2: "Cucumber",
+        choice3: "Orange",
+        choice4: "Pumpkin",
+        answer: 4
+    },
+    {
+        question: "Neville Longbottom can often be found looking for what sort of pet?",
+        choice1: "Frog",
+        choice2: "Dog",
+        choice3: "Rat",
+        choice4: "Toad",
+        answer: 4
+    },
+    {
+        question: "Ron Weasley is very good at which game?",
+        choice1: "Snap",
+        choice2: "Scrabble",
+        choice3: "Chess",
+        choice4: "Boggle",
+        answer: 3
+    },
+    {
+        question: "Hermione's parents are described as what?",
+        choice1: "Weird",
+        choice2: "Muggles",
+        choice3: "Old",
+        choice4: "Gnomes",
+        answer: 2
+    },
+    {
+        question: "What sort of pet has Hagrid always wanted?",
+        choice1: "A snake",
+        choice2: "A spider",
+        choice3: "A unicorn",
+        choice4: "A dragon",
+        answer: 4
+    },
+    {
+        question: "Professor Snape is head of which Hogwarts house?",
+        choice1: "Slytherin",
+        choice2: "Gryffindor",
+        choice3: "Ravenclaw",
+        choice4: "Hufflepuff",
+        answer: 1
+    },
+    {
+        question: "Professor Quirrell wears what sort of headwear?",
+        choice1: "A baseball cap",
+        choice2: "A bonnet",
+        choice3: "A turban",
+        choice4: "A swimming cap",
+        answer: 3
+    },
+    {
+        question: "Harry can talk to what kind of animals?",
+        choice1: "Snakes",
+        choice2: "Cats",
+        choice3: "Dolphins",
+        choice4: "Crows",
+        answer: 1
+    },
+    {
+        question: "What sort of creature is Dobby?",
+        choice1: "Goblin",
+        choice2: "Gnome",
+        choice3: "House elf",
+        choice4: "Dementor",
+        answer: 3
+    },
+]
